add8c7b0d1712fdc752688dd484d5e5c43bc4c16
Aarian Moogat <amoogat@gmail.com>
2020-06-12 01:15:34 -0700

    added geojson data and airport data

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,67 @@
+// creates a map centered at specified coords and a zoom level
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+/** Coordinates for a line */
+// let line = [
+// 	[33.9416, -118.4085],
+// 	[37.6213, -122.3790],
+// 	[40.7899, -111.9791],
+// 	[47.4502, -122.3088]
+// ];
+
+// Adds GeoJSON data
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Gets the GeoJSON data
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+	  layer.bindPopup("<h2>" + 'Airport code: ' + feature.properties.faa + "</h2>"
+	  + "<h3>" + 'Airport name: ' + feature.properties.name + "</h3>");
+    }
+}).addTo(map);
+/** Makes a yellow line connecting the coordinates */
+// L.polyline(line, {
+// 	color: "yellow"
+//  }).addTo(map);
+
+// // Get data from cities.js
+// let cityData = cities;
+
+// Loops through each city and makes markers
+// cityData.forEach(function(city) {
+// 	console.log(city)
+// 	L.circleMarker(city.location, {
+//         radius: city.population / 100000
+//     })
+// 	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+//   .addTo(map);
+// });
+
+// Creates background for map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// adds graymap tile layer to the map
+streets.addTo(map);
