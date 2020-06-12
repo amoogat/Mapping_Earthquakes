df6379c58146fe37a3f14878e9cc1a511a91f817
Aarian Moogat <amoogat@gmail.com>
2020-06-11 21:17:44 -0700

    added multiple point mapping

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,12 +4,17 @@ console.log("working");
 // creates a map centered at specified coords and a zoom level
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-//  Adds a marker on the map
-L.circleMarker([34.0522, -118.2437], {
-    radius: 100,
-    color: "black",
-    fillColor: '#ffffa1'
- }).addTo(map);
+// Get data from cities.js
+let cityData = cities;
+
+// Loops through each city and makes markers
+cityData.forEach(function(city) {
+	console.log(city)
+	L.marker(city.location)
+	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  .addTo(map);
+});
+
 // Creates background for map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
