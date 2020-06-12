edc76edadd5ae73b74eff0efff8d594979100698
Aarian Moogat <amoogat@gmail.com>
2020-06-12 09:57:50 -0700

    added layers and geoJSON points with descriptions

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,5 +1,5 @@
 // creates a map centered at specified coords and a zoom level
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+
 
 /** Coordinates for a line */
 // let line = [
@@ -9,35 +9,56 @@ let map = L.map('mapid').setView([37.5, -122.5], 10);
 // 	[47.4502, -122.3088]
 // ];
 
-// Adds GeoJSON data
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+// creates regular background for map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
 
-// Gets the GeoJSON data
-L.geoJson(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    onEachFeature: function(feature, layer) {
-      console.log(layer);
-	  layer.bindPopup("<h2>" + 'Airport code: ' + feature.properties.faa + "</h2>"
-	  + "<h3>" + 'Airport name: ' + feature.properties.name + "</h3>");
-    }
+// Creates dark background for map
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+
+// adds both tile layers to the map
+let baseMaps = {
+	Street: streets,
+	Dark: dark
+  };
+
+let map = L.map('mapid', {
+	center: [30, 30],
+	zoom: 2,
+	layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Adds GeoJSON data
+let airportData = "https://raw.githubusercontent.com/amoogat/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+
+// Gets the GeoJSON data for map
+// @param data is the airportData --> L.geoJSON --> addTo
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creates geoJSON layer for map
+  L.geoJson(data, {
+	// We turn each feature into a marker on the map.
+	onEachFeature: function (feature, layer) {
+		console.log(layer);
+		layer.bindPopup("<h2>" + 'Airport code: ' + feature.properties.faa + "</h2>"
+			+ "<h3>" + 'Airport name: ' + feature.properties.name + "</h3>");
+	}
 }).addTo(map);
+});
+
+
+
 /** Makes a yellow line connecting the coordinates */
 // L.polyline(line, {
 // 	color: "yellow"
@@ -56,12 +77,3 @@ L.geoJson(sanFranAirport, {
 //   .addTo(map);
 // });
 
-// Creates background for map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	accessToken: API_KEY
-});
-
-// adds graymap tile layer to the map
-streets.addTo(map);
